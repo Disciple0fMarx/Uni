ed2482fdc08f819ca7af1693427cca7c6c0a59ea
Dhya Eddine El Bahri <dhya.jake@gmail.com>
2021-11-03 17:41:53 +0100

    Finished TP2

--- TPs Sécurité Informatique/TP 2/exercice.c
+++ TPs Sécurité Informatique/TP 2/exercice.c
@@ -2,16 +2,22 @@
 
 
 int main() {
-    char key[] = "ERIC";
-    char message[] = "Abcdefghijklmnopqrstuvwxyz!!!";
+    char key[] = "BC";
+    char message[] = "Secret!!!";
 
     char encrypted[strlen(message)];
     cryptage_vigenere(message, encrypted, key);
-    printf("The encrypted message is '%s'\n", encrypted);
+    printf("Le message crypte est '%s'\n", encrypted);
     
     char decrypted[strlen(encrypted)];
     decryptage_vigenere(encrypted, decrypted, key);
-    printf("The decrypted message is '%s'\n", decrypted);
+    printf("Le message decrypte est '%s'\n", decrypted);
+
+    char keyGuess[strlen(key)];
+    printf(
+        "La cle d'apres la cryptoanalyse est '%s'\n",
+        cryptoanalyse_vigenere(message, encrypted, strlen(key))
+    );
 
     return 0;
 }
--- TPs Sécurité Informatique/TP 2/exercice.h
+++ TPs Sécurité Informatique/TP 2/exercice.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
 
 
 char decalage(char c, int k) {
@@ -21,19 +22,42 @@ char decalage(char c, int k) {
 }
 
 
+int map(char c) {
+    if (c >= 'A' && c <= 'Z') return c - 'A';
+    if (c >= 'a' && c <= 'z') return c - 'a';
+    return 0;
+}
+
+
 void cryptage_vigenere(char *in, char *out, char *k) {
     /* Permet de crypter 'in' avec le clé 'k'
      * et renvoie le résultat en 'out'.
      */
     int i, j = 0;
     for (i = 0; i < strlen(in); i ++) {
-            out[i] = decalage(in[i], k[j] - 'A');
-            if (isalpha(in[i])) {
-                j ++;
-                j %= strlen(k);
-            }
+        out[i] = decalage(in[i], map(k[j]));
+        if (isalpha(in[i])) {
+            j ++;
+            j %= strlen(k);
+        }
     }
     out[strlen(in)] = '\0';
+}
+
+
+char soustraire(char c, int k) {
+    /* La fonction inverse de 'decalage(char c, int k)'. */
+    char result;
+    if (c >= 'A' && c <= 'Z') {
+        result = (c - 'A' - k) % 26 + 'A';
+    } else {
+        if (c >= 'a' && c <= 'z') {
+            result = (c - 'a' - k) % 26 + 'a';
+        } else {
+            result = c;
+        }
+    }
+    return result;
 }
 
 
@@ -43,11 +67,42 @@ void decryptage_vigenere(char *in, char *out, char *k) {
      */
     int i, j = 0;
     for (i = 0; i < strlen(in); i ++) {
-            out[i] = decalage(in[i], -(k[j] - 'A'));
-            if (isalpha(in[i])) {
-                j ++;
-                j %= strlen(k);
-            }
+        out[i] = soustraire(in[i], map(k[j]));
+        if (isalpha(in[i])) {
+            j ++;
+            j %= strlen(k);
+        }
+        out[strlen(in)] = '\0';
     }
-    out[strlen(in)] = '\0';
+}
+
+
+char *cryptoanalyse_vigenere(char *in, char *out, int longueur) {
+    /* Permet de déterminer la clé qui crypte 'in' en 'out'
+     * s'il existe.
+     */
+    char temp[longueur];
+    char result[longueur];
+    int i = 0, j = 0;
+    do {
+        if (isalpha(in[i])) {
+            temp[j] = in[i];
+            j ++;
+        }
+        i ++;
+    } while (i < longueur);
+    temp[longueur] = '\0';
+    for (i = 0; i < longueur; i ++) {
+        int j = 0, found = 0;
+        char aux;
+        do {
+            aux = decalage(temp[i], j);
+            j ++;
+            if (out[i] == aux) found = 1;
+        } while (!found && j < 26);
+        result[i] = j - 1 + 'A';
+    }
+    result[longueur] = '\0';
+    char *output = result;
+    return output;
 }
