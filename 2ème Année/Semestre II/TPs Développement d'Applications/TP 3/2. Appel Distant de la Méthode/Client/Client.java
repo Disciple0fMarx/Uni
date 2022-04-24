3b264910477d3b9f7c1d31aca4b45fdb2f9f8f9e
Dhya Eddine El Bahri <dhya.jake@gmail.com>
2022-04-24 16:56:04 +0100

    Fixed errors in TP 3 (App Dev)

--- 2ème Année/Semestre II/TPs Développement d'Applications/TP 3/2. Appel Distant de la Méthode/Client/Client.java
+++ 2ème Année/Semestre II/TPs Développement d'Applications/TP 3/2. Appel Distant de la Méthode/Client/Client.java
@@ -1,11 +1,18 @@
 import java.rmi.RemoteException;
+// import java.rmi.RMISecurityManager;
 import java.rmi.Naming;
 
 
 public class Client {
     public static void main(String[] args) throws RemoteException {
-        InverseInterface object = (InverseInterface) Naming.lookup("rmi://localhost:1099/myObject");
+        try {
+            // System.setSecurityManager(new RMISecurityManager());
 
-        System.out.println(object.reverseString("Salut"));
+            InverseInterface object = (InverseInterface) Naming.lookup("rmi://localhost/myObject");
+
+            System.out.println(object.reverseString("Salut"));
+        } catch (Exception e) {
+            System.out.println("\n[" + e.toString() + "]\n");
+        }
     }
 }
--- 2ème Année/Semestre II/TPs Développement d'Applications/TP 3/2. Appel Distant de la Méthode/Server/Server.java
+++ 2ème Année/Semestre II/TPs Développement d'Applications/TP 3/2. Appel Distant de la Méthode/Server/Server.java
@@ -1,15 +1,25 @@
-import java.rmi.RemoteException;
+// import java.rmi.RemoteException;
+import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.Naming;
 
+import java.net.MalformedURLException;
+
 public class Server {
-    public static void main(String[] args) throws RemoteException {
-        // Création de la service de nommage
-        LocateRegistry.createRegistry(1099);
+    public static void main(String[] args) {
+        try {
+            // Création de la service de nommage
+            LocateRegistry.createRegistry(1099);
+            System.out.println("\n[Created registry in port 1099]\n");
 
-        // Création d'objet
-        InverseInterface object = new Inverse();
+            // Création d'objet
+            InverseInterface object = new Inverse();
+            System.out.println("\n[Created instance of object]\n");
 
-        Naming.rebind("rmi://localhost:1099/myObject", object);
+            Naming.rebind("rmi://localhost:1099/myObject", object);
+            System.out.println("\n[Added object to rmiregister]\n");
+        } catch (Exception e) {
+            System.out.println("\n[" + e.toString() + "]\n");
+        }
     }
 }
